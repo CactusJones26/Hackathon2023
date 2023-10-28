ce10a63bebc9dda7c0713422d045bf71bb8c9362
Kevin Dong <KDong2002@outlook.com>
2023-10-28 12:33:07 -0400

    Initial Commit

---
+++ HackOhio/HackOhio/HackOhioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackOhioApp.swift
+//  HackOhio
+//
+//  Created by Kevin Dong on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HackOhioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
